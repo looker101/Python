b20db876660e5d4eac487956eed9354a5829a487
looker101 <lookeronline@gmail.com>
2023-10-09 16:21:00 +0200

    file off white inserito

---
+++ Off_White/compare_quantity.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+try:
+    class Off_White:
+        def __init__(self, file_negozi, file_shopify):
+            self.file_negozi = file_negozi
+            self.file_shopify = file_shopify
+            
+        # creo il modulo bottega e assemblo il file relativo ai prodotto nei negozi    
+        def bottega(self):
+            negozi = pd.read_excel("Negozi.xlsx", index_col="Marchio")
+            negozi.drop(columns=["Codice magazzino", "Tipo prodotto", "Fornitore", "Linea", "Sfera", "Cilindro", 
+                        "Diametro", "Rb", "Tipo lenti", "Materiale", "Trattamento", "Descrizione", "Colore 2",
+                        "Utente", "Ponte", "Calibro", "Asta", "Codice filiale", "Prezzo acquisto", "Valore magazzino",
+                        "Data ult. acq", "Prezzo saldo", "Magazzino", "Filiale2", "Colore"
+                        ], inplace = True)
+            negozi.rename(columns = {"Codice a barre":"Barcode"}, inplace = True)
+            return negozi
+        
+        # creo il modulo bottega e assemblo il file relativo ai prodotto nei negozi
+        def online(self):
+            shopify = pd.read_excel("Shopify.xlsx", index_col="Vendor")
+            shopify.drop(columns=[
+                "ID", "Handle", "Command", "Body HTML", "Type", "Tags", "Tags Command", "Created At", "Updated At",
+                "Status", "Published", "Published At", "Template Suffix", "Variant ID", "Variant Inventory Qty", "Variant Inventory Adjust",
+                "Variant Price", "Variant Compare At Price", "URL"], inplace = True)
+            shopify.rename(columns = {"Variant Barcode" : "Barcode", "Inventory Available: +39 05649689443" : "Q.ty_Looker"}, inplace = True)
+            return shopify
+        
+        def confronto(self):
+            negozi = neg.bottega()
+            shopify = neg.online()
+            compare = shopify.merge(negozi, how = "inner", on = "Barcode", suffixes = ("_Looker", "_Negozi"))
+            compare.set_index("Title", inplace = True)
+            compare = compare[["Barcode", "Filiale", "Variant SKU", "Q.ty_Looker", "Quantità magazzino"]]
+            return compare
+        
+    # crezione della variabile "path" a cui assegno il percordo per aprire il file
+    path_negozi = r"C:\Users\miche\Documents\GitHub\Off_White\Negozi.xlsx"
+    path_shopify = r"C:\Users\miche\Documents\GitHub\Off_White\Shopify.xlsx"
+
+    neg = Off_White(path_negozi, path_shopify)
+
+    #neg.bottega()
+    #neg.online()
+    compare_at = neg.confronto()
+
+    compare_at.to_excel("off_white_compare.xlsx")
+    print("File stampato nella directory!")
+except Exception as e: 
+    print(f"Si è verificato un errore {e}")
